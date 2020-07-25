1a28aa4210bba5a04357e544c4be9930193abd84
vrudas <vrudas4work@gmail.com>
2020-07-25 23:48:05 +0300

    Migrated MidiProtocolClient to Kotlin

--- lp4k-midi/src/main/kotlin/io/lp4k/midi/protocol/MidiProtocolClient.kt
+++ lp4k-midi/src/main/kotlin/io/lp4k/midi/protocol/MidiProtocolClient.kt
@@ -15,30 +15,29 @@
  *    limitations under the License.
  *
  */
+package io.lp4k.midi.protocol
 
-package io.lp4k.midi.protocol;
-
-import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.InvalidMidiDataException
 
 /**
  * A low-level client used to send raw MIDI commands to a physical Launchpad device.
  *
- * <p>Please note that some that the Launchpad considers some of the (round) buttons as (square) pads, whereas some
+ * Please note that some that the Launchpad considers some of the (round) buttons as (square) pads, whereas some
  * other buttons are treated as, well, buttons. This explains some oddities in the method signatures.
  *
- * <p>For the full specification and details about the meaning of each command and their acceptable parameter values,
+ * For the full specification and details about the meaning of each command and their acceptable parameter values,
  * please refer to Novation's online documentation.
  *
  * @author Olivier Croisier (olivier.croisier@gmail.com)
  */
-public interface MidiProtocolClient {
-
+interface MidiProtocolClient {
     /**
      * Sends a "reset" command.
      *
      * @throws InvalidMidiDataException If a MIDI communication error occurs.
      */
-    void reset() throws InvalidMidiDataException;
+    @Throws(InvalidMidiDataException::class)
+    fun reset()
 
     /**
      * Sends a "lights on" command, lightning all the lights at once. Useful to test the LEDs.
@@ -46,7 +45,8 @@ public interface MidiProtocolClient {
      * @param intensity The desired light intensity.
      * @throws InvalidMidiDataException If a MIDI communication error occurs.
      */
-    void lightsOn(int intensity) throws InvalidMidiDataException;
+    @Throws(InvalidMidiDataException::class)
+    fun lightsOn(intensity: Int)
 
     /**
      * Sends a "note on" command, to light up the LED under a pad or button.
@@ -55,7 +55,8 @@ public interface MidiProtocolClient {
      * @param color The color to display.
      * @throws InvalidMidiDataException If a MIDI communication error occurs.
      */
-    void noteOn(int note, int color) throws InvalidMidiDataException;
+    @Throws(InvalidMidiDataException::class)
+    fun noteOn(note: Int, color: Int)
 
     /**
      * Sends a "note off" command, to turn off the LED under a pad or button.
@@ -63,7 +64,8 @@ public interface MidiProtocolClient {
      * @param note The "note" identifying the target pad or button.
      * @throws InvalidMidiDataException If a MIDI communication error occurs.
      */
-    void noteOff(int note) throws InvalidMidiDataException;
+    @Throws(InvalidMidiDataException::class)
+    fun noteOff(note: Int)
 
     /**
      * A batch version of the "light on" command. Multiple lights can be set at once, starting from the upper-left one.
@@ -71,7 +73,8 @@ public interface MidiProtocolClient {
      * @param colors The colors to display.
      * @throws InvalidMidiDataException If a MIDI communication error occurs.
      */
-    void notesOn(int... colors) throws InvalidMidiDataException;
+    @Throws(InvalidMidiDataException::class)
+    fun notesOn(vararg colors: Int)
 
     /**
      * Toggles between a X-Y layout and "note-oriented" one.
@@ -79,7 +82,8 @@ public interface MidiProtocolClient {
      * @param mode The layout to switch to.
      * @throws InvalidMidiDataException If a MIDI communication error occurs.
      */
-    void layout(int mode) throws InvalidMidiDataException;
+    @Throws(InvalidMidiDataException::class)
+    fun layout(mode: Int)
 
     /**
      * Sends a "button on" command, to light up the LED under a button.
@@ -88,7 +92,8 @@ public interface MidiProtocolClient {
      * @param color  The desired color.
      * @throws InvalidMidiDataException If a MIDI communication error occurs.
      */
-    void buttonOn(int button, int color) throws InvalidMidiDataException;
+    @Throws(InvalidMidiDataException::class)
+    fun buttonOn(button: Int, color: Int)
 
     /**
      * Sets the overall brightness of the LEDs, expressed as a ratio between a numerator and a denominator.
@@ -97,7 +102,8 @@ public interface MidiProtocolClient {
      * @param denominator The denominator.
      * @throws InvalidMidiDataException If a MIDI communication error occurs.
      */
-    void brightness(int numerator, int denominator) throws InvalidMidiDataException;
+    @Throws(InvalidMidiDataException::class)
+    fun brightness(numerator: Int, denominator: Int)
 
     /**
      * Scrolls the given text across the board, using an embedded 8x8 dot matrix font.
@@ -108,7 +114,13 @@ public interface MidiProtocolClient {
      * @param loop  Whether to display the text once, or to loop until a new "text" command is emitted.
      * @throws InvalidMidiDataException If a MIDI communication error occurs.
      */
-    void text(String text, int color, int speed, boolean loop) throws InvalidMidiDataException;
+    @Throws(InvalidMidiDataException::class)
+    fun text(
+        text: String?,
+        color: Int,
+        speed: Int,
+        loop: Boolean
+    )
 
     /**
      * Sets which buffer is written to, and which one is currently displayed (can be the same).
@@ -118,9 +130,15 @@ public interface MidiProtocolClient {
      * @param visibleBuffer                  The buffer to set as the visible buffer.
      * @param writeBuffer                    The buffer to set as the write buffer.
      * @param copyVisibleBufferToWriteBuffer Whether the visible buffer should be copied to the back buffer during the
-     *                                       process.
-     * @param autoSwap                       Whether to trigger the "blinking" effect. Set to {@code false} to disable blinking.
+     * process.
+     * @param autoSwap                       Whether to trigger the "blinking" effect. Set to `false` to disable blinking.
      * @throws InvalidMidiDataException If a MIDI communication error occurs.
      */
-    void doubleBufferMode(int visibleBuffer, int writeBuffer, boolean copyVisibleBufferToWriteBuffer, boolean autoSwap) throws InvalidMidiDataException;
+    @Throws(InvalidMidiDataException::class)
+    fun doubleBufferMode(
+        visibleBuffer: Int,
+        writeBuffer: Int,
+        copyVisibleBufferToWriteBuffer: Boolean,
+        autoSwap: Boolean
+    )
 }
