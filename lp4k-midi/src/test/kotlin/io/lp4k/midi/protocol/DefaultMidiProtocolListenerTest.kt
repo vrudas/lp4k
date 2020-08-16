c771018aac1c7464fbb720faab294b4115b14722
vrudas <vrudas4work@gmail.com>
2020-08-17 00:09:00 +0300

    Migrated DefaultMidiProtocolListenerTest to Kotlin

--- lp4k-midi/src/test/kotlin/io/lp4k/midi/protocol/DefaultMidiProtocolListenerTest.kt
+++ lp4k-midi/src/test/kotlin/io/lp4k/midi/protocol/DefaultMidiProtocolListenerTest.kt
@@ -15,131 +15,110 @@
  *    limitations under the License.
  *
  */
+package io.lp4k.midi.protocol
 
-package io.lp4k.midi.protocol;
+import net.thecodersbreakfast.lp4j.api.Button
+import net.thecodersbreakfast.lp4j.api.LaunchpadListener
+import net.thecodersbreakfast.lp4j.api.Pad
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
 
-import net.thecodersbreakfast.lp4j.api.Button;
-import net.thecodersbreakfast.lp4j.api.LaunchpadListener;
-import net.thecodersbreakfast.lp4j.api.Pad;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-public class DefaultMidiProtocolListenerTest {
-
-    private static final long TIMESTAMP = -1;
-
-    private static final int BUTTON_UP = 104;
-    private static final int BUTTON_UNKNOWN = 100;
-    private static final int NOTE_VOL = 8;
-    private static final int NOTE_PAD00 = 0;
-    private static final int NOTE_UNKNOWN = -1;
-
-    private MidiProtocolListener midiProtocolListener;
-    private LaunchpadListener listener;
+class DefaultMidiProtocolListenerTest {
+    
+    private lateinit var midiProtocolListener: MidiProtocolListener
+    private lateinit var listener: LaunchpadListener
 
     @BeforeEach
-    public void init() {
-        this.listener = Mockito.mock(LaunchpadListener.class);
-        this.midiProtocolListener = new DefaultMidiProtocolListener(listener);
+    fun init() {
+        listener = mock(LaunchpadListener::class.java)
+        midiProtocolListener = DefaultMidiProtocolListener(listener)
     }
 
     @Test
-    void default_midi_protocol_listener_not_created_because_of_null_launchpad_listener() {
-        assertThrows(
-            IllegalArgumentException.class,
-            () -> new DefaultMidiProtocolListener(null)
-        );
+    fun onButtonOn_up() {
+        midiProtocolListener.onButtonOn(BUTTON_UP, TIMESTAMP)
+        verify(listener).onButtonPressed(Button.UP, TIMESTAMP)
     }
 
     @Test
-    public void onButtonOn_up() {
-        midiProtocolListener.onButtonOn(BUTTON_UP, TIMESTAMP);
-        Mockito.verify(listener).onButtonPressed(Button.UP, TIMESTAMP);
+    fun onButtonOn_unknown() {
+        assertThrows(IllegalArgumentException::class.java) {
+            midiProtocolListener.onButtonOn(BUTTON_UNKNOWN, TIMESTAMP)
+            verify(listener).onButtonPressed(Button.UP, TIMESTAMP)
+        }
     }
 
     @Test
-    public void onButtonOn_unknown() {
-        assertThrows(
-            IllegalArgumentException.class,
-            () -> {
-                midiProtocolListener.onButtonOn(BUTTON_UNKNOWN, TIMESTAMP);
-                Mockito.verify(listener).onButtonPressed(Button.UP, TIMESTAMP);
-            }
-        );
+    fun onButtonOff_up() {
+        midiProtocolListener.onButtonOff(BUTTON_UP, TIMESTAMP)
+        verify(listener).onButtonReleased(Button.UP, TIMESTAMP)
     }
 
     @Test
-    public void onButtonOff_up() {
-        midiProtocolListener.onButtonOff(BUTTON_UP, TIMESTAMP);
-        Mockito.verify(listener).onButtonReleased(Button.UP, TIMESTAMP);
+    fun onButtonOff_unknown() {
+        assertThrows(IllegalArgumentException::class.java) {
+            midiProtocolListener.onButtonOff(BUTTON_UNKNOWN, TIMESTAMP)
+            verify(listener).onButtonReleased(Button.UP, TIMESTAMP)
+        }
     }
 
     @Test
-    public void onButtonOff_unknown() {
-        assertThrows(
-            IllegalArgumentException.class,
-            () -> {
-                midiProtocolListener.onButtonOff(BUTTON_UNKNOWN, TIMESTAMP);
-                Mockito.verify(listener).onButtonReleased(Button.UP, TIMESTAMP);
-            }
-        );
+    fun onNoteOn_vol() {
+        midiProtocolListener.onNoteOn(NOTE_VOL, TIMESTAMP)
+        verify(listener).onButtonPressed(Button.VOL, TIMESTAMP)
     }
 
     @Test
-    public void onNoteOn_vol() {
-        midiProtocolListener.onNoteOn(NOTE_VOL, TIMESTAMP);
-        Mockito.verify(listener).onButtonPressed(Button.VOL, TIMESTAMP);
+    fun onNoteOff_vol() {
+        midiProtocolListener.onNoteOff(NOTE_VOL, TIMESTAMP)
+        verify(listener).onButtonReleased(Button.VOL, TIMESTAMP)
     }
 
     @Test
-    public void onNoteOff_vol() {
-        midiProtocolListener.onNoteOff(NOTE_VOL, TIMESTAMP);
-        Mockito.verify(listener).onButtonReleased(Button.VOL, TIMESTAMP);
+    fun onNoteOn_pad00() {
+        midiProtocolListener.onNoteOn(NOTE_PAD00, TIMESTAMP)
+        verify(listener).onPadPressed(Pad.at(0, 0), TIMESTAMP)
     }
 
     @Test
-    public void onNoteOn_pad00() {
-        midiProtocolListener.onNoteOn(NOTE_PAD00, TIMESTAMP);
-        Mockito.verify(listener).onPadPressed(Pad.at(0, 0), TIMESTAMP);
+    fun onNoteOff_pad00() {
+        midiProtocolListener.onNoteOff(NOTE_PAD00, TIMESTAMP)
+        verify(listener).onPadReleased(Pad.at(0, 0), TIMESTAMP)
     }
 
     @Test
-    public void onNoteOff_pad00() {
-        midiProtocolListener.onNoteOff(NOTE_PAD00, TIMESTAMP);
-        Mockito.verify(listener).onPadReleased(Pad.at(0, 0), TIMESTAMP);
+    fun onNoteOn_unknown() {
+        assertThrows(IllegalArgumentException::class.java) {
+            midiProtocolListener.onNoteOn(NOTE_UNKNOWN, TIMESTAMP)
+            verify(listener).onPadPressed(Pad.at(0, 0), TIMESTAMP)
+        }
     }
 
     @Test
-    public void onNoteOn_unknown() {
-        assertThrows(
-            IllegalArgumentException.class,
-            () -> {
-                midiProtocolListener.onNoteOn(NOTE_UNKNOWN, TIMESTAMP);
-                Mockito.verify(listener).onPadPressed(Pad.at(0, 0), TIMESTAMP);
-            }
-        );
+    fun onNoteOff_unknown() {
+        assertThrows(IllegalArgumentException::class.java) {
+            midiProtocolListener.onNoteOff(NOTE_UNKNOWN, TIMESTAMP)
+            verify(listener).onPadReleased(Pad.at(0, 0), TIMESTAMP)
+        }
     }
 
     @Test
-    public void onNoteOff_unknown() {
-        assertThrows(
-            IllegalArgumentException.class,
-            () -> {
-                midiProtocolListener.onNoteOff(NOTE_UNKNOWN, TIMESTAMP);
-                Mockito.verify(listener).onPadReleased(Pad.at(0, 0), TIMESTAMP);
-            }
-        );
-
+    fun onTextScrolled() {
+        midiProtocolListener.onTextScrolled(TIMESTAMP)
+        verify(listener).onTextScrolled(TIMESTAMP)
     }
 
-    @Test
-    public void onTextScrolled() {
-        midiProtocolListener.onTextScrolled(TIMESTAMP);
-        Mockito.verify(listener).onTextScrolled(TIMESTAMP);
+    companion object {
+        private const val TIMESTAMP: Long = -1
+        private const val BUTTON_UP = 104
+        private const val BUTTON_UNKNOWN = 100
+        private const val NOTE_VOL = 8
+        private const val NOTE_PAD00 = 0
+        private const val NOTE_UNKNOWN = -1
     }
-
-
 }
