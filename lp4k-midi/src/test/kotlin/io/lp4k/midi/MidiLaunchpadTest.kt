d39293f2fc4e1632888f99e174b384ebe428853a
vrudas <vrudas4work@gmail.com>
2020-09-06 22:01:32 +0300

    Migrated MidiLaunchpadTest to Kotlin

--- lp4k-midi/src/test/kotlin/io/lp4k/midi/MidiLaunchpadTest.kt
+++ lp4k-midi/src/test/kotlin/io/lp4k/midi/MidiLaunchpadTest.kt
@@ -14,297 +14,239 @@
  *    limitations under the License.
  *
  */
+package io.lp4k.midi
 
-package io.lp4k.midi;
+import io.lp4k.midi.protocol.DefaultMidiProtocolReceiver
+import net.thecodersbreakfast.lp4j.api.Launchpad
+import net.thecodersbreakfast.lp4j.api.LaunchpadListenerAdapter
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.*
+import org.mockito.junit.jupiter.MockitoExtension
+import javax.sound.midi.MidiDevice
 
-import io.lp4k.midi.protocol.DefaultMidiProtocolReceiver;
-import net.thecodersbreakfast.lp4j.api.Launchpad;
-import net.thecodersbreakfast.lp4j.api.LaunchpadException;
-import net.thecodersbreakfast.lp4j.api.LaunchpadListenerAdapter;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+@ExtendWith(MockitoExtension::class)
+internal class MidiLaunchpadTest {
 
-import javax.sound.midi.MidiDevice;
-import javax.sound.midi.MidiUnavailableException;
-import java.io.IOException;
+    private lateinit var launchpad: Launchpad
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.*;
+    private fun mockDeviceConfiguration(
+        inputDevice: MidiDevice,
+        outputDevice: MidiDevice
+    ): MidiDeviceConfiguration {
+        val deviceConfiguration = mock(MidiDeviceConfiguration::class.java)
 
-@ExtendWith(MockitoExtension.class)
-class MidiLaunchpadTest {
+        `when`(deviceConfiguration.inputDevice).thenReturn(inputDevice)
+        `when`(deviceConfiguration.outputDevice).thenReturn(outputDevice)
 
-    private Launchpad launchpad;
-
-    private MidiDeviceConfiguration mockEmptyDeviceConfiguration() {
-        return mockDeviceConfiguration(
-            null,
-            null
-        );
+        return deviceConfiguration
     }
 
-    private MidiDeviceConfiguration mockDeviceConfiguration(
-        MidiDevice inputDevice,
-        MidiDevice outputDevice
-    ) {
-        var deviceConfiguration = mock(MidiDeviceConfiguration.class);
+    private fun mockOutputDevice(isOpen: Boolean): FakeMidiDevice {
+        val receiver = mock(FakeReceiver::class.java)
+        val outputDevice = mock(FakeMidiDevice::class.java)
 
-        when(deviceConfiguration.getInputDevice()).thenReturn(inputDevice);
+        `when`(outputDevice.isOpen).thenReturn(isOpen)
+        `when`(outputDevice.receiver).thenReturn(receiver)
 
-        when(deviceConfiguration.getOutputDevice()).thenReturn(outputDevice);
-
-        return deviceConfiguration;
+        return outputDevice
     }
 
-    private FakeMidiDevice mockOutputDevice(boolean isOpen) throws MidiUnavailableException {
-        var receiver = mock(FakeReceiver.class);
+    private fun mockInputDevice(isOpen: Boolean): FakeMidiDevice {
+        val transmitter = mock(FakeTransmitter::class.java)
+        val inputDevice = mock(FakeMidiDevice::class.java)
 
-        var outputDevice = mock(FakeMidiDevice.class);
+        `when`(inputDevice.isOpen).thenReturn(isOpen)
+        `when`(inputDevice.transmitter).thenReturn(transmitter)
 
-        when(outputDevice.isOpen()).thenReturn(isOpen);
-
-        when(outputDevice.getReceiver()).thenReturn(receiver);
-
-        return outputDevice;
-    }
-
-    private FakeMidiDevice mockInputDevice(boolean isOpen) throws MidiUnavailableException {
-        var transmitter = mock(FakeTransmitter.class);
-
-        var inputDevice = mock(FakeMidiDevice.class);
-
-        when(inputDevice.isOpen()).thenReturn(isOpen);
-        when(inputDevice.getTransmitter()).thenReturn(transmitter);
-
-        return inputDevice;
+        return inputDevice
     }
 
     @Test
-    void failed_to_create_midi_launchpad_because_of_null_midi_configuration() {
-        assertThrows(
-            IllegalArgumentException.class,
-            () -> new MidiLaunchpad(null)
-        );
-    }
+    fun midi_launchpad_created_with_already_opened_receiver() {
+        val outputDevice = mockOutputDevice(true)
 
-    @Test
-    @Disabled("as after migration to Kotlin situation is not reachable")
-    void get_client_failed_because_of_null_receiver() throws MidiUnavailableException {
-        var launchpad = new MidiLaunchpad(
-            mockEmptyDeviceConfiguration()
-        );
-
-        assertThrows(
-            LaunchpadException.class,
-            launchpad::getClient
-        );
-    }
-
-    @Test
-    @Disabled("as after migration to Kotlin situation is not reachable")
-    void set_listener_failed_because_of_null_transmitter() throws MidiUnavailableException {
-        var launchpad = new MidiLaunchpad(
-            mockEmptyDeviceConfiguration()
-        );
-
-        assertThrows(
-            LaunchpadException.class,
-            () -> launchpad.setListener(
-                new LaunchpadListenerAdapter() {
-                }
-            )
-        );
-    }
-
-    @Test
-    void midi_launchpad_created_with_already_opened_receiver() throws MidiUnavailableException {
-        var outputDevice = mockOutputDevice(true);
-
-        launchpad = new MidiLaunchpad(
+        launchpad = MidiLaunchpad(
             mockDeviceConfiguration(
                 mockInputDevice(true),
                 outputDevice
             )
-        );
+        )
 
-        verify(outputDevice, never()).open();
+        verify(outputDevice, never()).open()
     }
 
     @Test
-    void midi_launchpad_created_with_not_opened_receiver() throws MidiUnavailableException {
-        var outputDevice = mockOutputDevice(false);
+    fun midi_launchpad_created_with_not_opened_receiver() {
+        val outputDevice = mockOutputDevice(false)
 
-        launchpad = new MidiLaunchpad(
+        launchpad = MidiLaunchpad(
             mockDeviceConfiguration(
                 mockInputDevice(false),
                 outputDevice
             )
-        );
+        )
 
-        verify(outputDevice).open();
+        verify(outputDevice).open()
     }
 
     @Test
-    void midi_launchpad_created_with_already_opened_transmitter() throws MidiUnavailableException {
-        var inputDevice = mockInputDevice(true);
+    fun midi_launchpad_created_with_already_opened_transmitter() {
+        val inputDevice = mockInputDevice(true)
 
-        launchpad = new MidiLaunchpad(
+        launchpad = MidiLaunchpad(
             mockDeviceConfiguration(
                 inputDevice,
                 mockOutputDevice(false)
             )
-        );
+        )
 
-        verify(inputDevice, never()).open();
+        verify(inputDevice, never()).open()
     }
 
     @Test
-    void midi_launchpad_created_with_not_opened_transmitter() throws MidiUnavailableException {
-        var inputDevice = mockInputDevice(false);
+    fun midi_launchpad_created_with_not_opened_transmitter() {
+        val inputDevice = mockInputDevice(false)
 
-        launchpad = new MidiLaunchpad(
+        launchpad = MidiLaunchpad(
             mockDeviceConfiguration(
                 inputDevice,
                 mockOutputDevice(false)
             )
-        );
+        )
 
-        verify(inputDevice).open();
+        verify(inputDevice).open()
     }
 
     @Test
-    void launchpad_client_was_returned() throws MidiUnavailableException {
-        launchpad = new MidiLaunchpad(
+    fun launchpad_client_was_returned() {
+        launchpad = MidiLaunchpad(
             mockDeviceConfiguration(
                 mockInputDevice(false),
                 mockOutputDevice(false)
             )
-        );
+        )
 
-        assertNotNull(launchpad.getClient());
+        assertNotNull(launchpad.client)
     }
 
     @Test
-    void listener_set_was_successful() throws MidiUnavailableException {
-        var transmitter = mock(FakeTransmitter.class);
+    fun listener_set_was_successful() {
+        val transmitter = mock(FakeTransmitter::class.java)
+        val inputDevice = mockInputDevice(true)
 
-        var inputDevice = mockInputDevice(true);
-        when(inputDevice.getTransmitter()).thenReturn(transmitter);
+        `when`(inputDevice.transmitter).thenReturn(transmitter)
 
-        var deviceConfiguration = mockDeviceConfiguration(
+        val deviceConfiguration = mockDeviceConfiguration(
             inputDevice,
             mockOutputDevice(true)
-        );
+        )
 
-        launchpad = new MidiLaunchpad(deviceConfiguration);
+        launchpad = MidiLaunchpad(deviceConfiguration)
+        launchpad.setListener(object : LaunchpadListenerAdapter() {})
 
-        launchpad.setListener(new LaunchpadListenerAdapter() {
-        });
-
-        verify(transmitter).setReceiver(any(DefaultMidiProtocolReceiver.class));
+        verify(transmitter).receiver = any(DefaultMidiProtocolReceiver::class.java)
     }
 
     @Test
-    void output_device_was_not_closed_because_device_is_not_open() throws MidiUnavailableException, IOException {
-        var outputDevice = mockOutputDevice(false);
+    fun output_device_was_not_closed_because_device_is_not_open() {
+        val outputDevice = mockOutputDevice(false)
 
-        launchpad = new MidiLaunchpad(
+        launchpad = MidiLaunchpad(
             mockDeviceConfiguration(
                 mockInputDevice(false),
                 outputDevice
             )
-        );
+        )
 
-        launchpad.close();
+        launchpad.close()
 
-        verify(outputDevice, never()).close();
+        verify(outputDevice, never()).close()
     }
 
     @Test
-    void output_device_was_closed() throws MidiUnavailableException, IOException {
-        var outputDevice = mockOutputDevice(true);
+    fun output_device_was_closed() {
+        val outputDevice = mockOutputDevice(true)
 
-        launchpad = new MidiLaunchpad(
+        launchpad = MidiLaunchpad(
             mockDeviceConfiguration(
                 mockInputDevice(true),
                 outputDevice
             )
-        );
+        )
 
-        launchpad.close();
+        launchpad.close()
 
-        verify(outputDevice).close();
+        verify(outputDevice).close()
     }
 
     @Test
-    void input_device_was_not_closed_because_device_is_not_open() throws MidiUnavailableException, IOException {
-        var inputDevice = mockInputDevice(false);
+    fun input_device_was_not_closed_because_device_is_not_open() {
+        val inputDevice = mockInputDevice(false)
 
-        launchpad = new MidiLaunchpad(
+        launchpad = MidiLaunchpad(
             mockDeviceConfiguration(
                 inputDevice,
                 mockOutputDevice(false)
             )
-        );
+        )
 
-        launchpad.close();
+        launchpad.close()
 
-        verify(inputDevice, never()).close();
+        verify(inputDevice, never()).close()
     }
 
     @Test
-    void input_device_was_closed() throws MidiUnavailableException, IOException {
-        var inputDevice = mockInputDevice(true);
+    fun input_device_was_closed() {
+        val inputDevice = mockInputDevice(true)
 
-        launchpad = new MidiLaunchpad(
+        launchpad = MidiLaunchpad(
             mockDeviceConfiguration(
                 inputDevice,
                 mockOutputDevice(false)
             )
-        );
+        )
 
-        launchpad.close();
+        launchpad.close()
 
-        verify(inputDevice).close();
+        verify(inputDevice).close()
     }
 
     @Test
-    void io_devices_was_not_closed_because_devices_are_not_open() throws MidiUnavailableException, IOException {
-        var inputDevice = mockInputDevice(false);
-        var outputDevice = mockOutputDevice(false);
+    fun io_devices_was_not_closed_because_devices_are_not_open() {
+        val inputDevice = mockInputDevice(false)
+        val outputDevice = mockOutputDevice(false)
 
-        launchpad = new MidiLaunchpad(
+        launchpad = MidiLaunchpad(
             mockDeviceConfiguration(
                 inputDevice,
                 outputDevice
             )
-        );
+        )
 
-        launchpad.close();
+        launchpad.close()
 
-        verify(inputDevice, never()).close();
-        verify(outputDevice, never()).close();
+        verify(inputDevice, never()).close()
+        verify(outputDevice, never()).close()
     }
 
     @Test
-    void io_devices_was_closed() throws MidiUnavailableException, IOException {
-        var inputDevice = mockInputDevice(true);
-        var outputDevice = mockOutputDevice(true);
+    fun io_devices_was_closed() {
+        val inputDevice = mockInputDevice(true)
+        val outputDevice = mockOutputDevice(true)
 
-        launchpad = new MidiLaunchpad(
+        launchpad = MidiLaunchpad(
             mockDeviceConfiguration(
                 inputDevice,
                 outputDevice
             )
-        );
+        )
 
-        launchpad.close();
+        launchpad.close()
 
-        verify(inputDevice).close();
-        verify(outputDevice).close();
+        verify(inputDevice).close()
+        verify(outputDevice).close()
     }
-
 }
