753b0bdd1727bfef1b03b2bb03b11e2c39d0c971
vrudas <vrudas4work@gmail.com>
2020-09-07 20:47:01 +0300

    Migrated Brightness to Kotlin

--- lp4k-api/src/main/java/net/thecodersbreakfast/lp4j/api/Brightness.kt
+++ lp4k-api/src/main/java/net/thecodersbreakfast/lp4j/api/Brightness.kt
@@ -1,95 +1,82 @@
 /*
- * Copyright 2015 Olivier Croisier (thecodersbreakfast.net)
+ *    Copyright 2020 Vasyl Rudas
+ *    Copyright 2015 Olivier Croisier (thecodersbreakfast.net)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
  */
-
-package net.thecodersbreakfast.lp4j.api;
+package net.thecodersbreakfast.lp4j.api
 
 /**
  * Describes the level of brightness of the pads and buttons lights.
  *
- * <p>{@code Brightness} instances are immutable and cached.
+ * `Brightness` instances are immutable and cached.
+ *
+ * @param brightnessLevel The desired level of brightess.
  *
  * @author Olivier Croisier (olivier.croisier@gmail.com)
  */
-public final class Brightness {
+data class Brightness constructor(
+    val brightnessLevel: Int
+) {
+    companion object {
+        /** Minimum level of brightness  */
+        const val MIN_VALUE = 0
 
-    /** Minimum level of brightness */
-    public static final int MIN_VALUE = 0;
-    /** Maximum level of brightness */
-    public static final int MAX_VALUE = 15;
+        /** Maximum level of brightness  */
+        const val MAX_VALUE = 15
 
-    /** Cache for all levels of brightness */
-    private static final Brightness[] CACHE = new Brightness[16];
+        /** Cache for all levels of brightness  */
+        private val CACHE = Array(16) { Brightness(it) }
 
-    static {
-        for (int i = 0; i < 16; i++) {
-            CACHE[i] = new Brightness(i);
+        /** Minimum brightness  */
+        val BRIGHTNESS_MIN = of(MIN_VALUE)
+
+        /** Maximum brightness  */
+        val BRIGHTNESS_MAX = of(MAX_VALUE)
+
+        /**
+         * Factory method.
+         *
+         * @param brightness The desired level of brightess. Must be in range [[net.thecodersbreakfast.lp4j.api.Brightness.MIN_VALUE],[net.thecodersbreakfast.lp4j.api.Brightness.MAX_VALUE]]
+         * @return The Brightness instance.
+         * @throws java.lang.IllegalArgumentException If the requested brightness level is out of acceptable range.
+         */
+        fun of(brightness: Int): Brightness {
+            require(brightness in (MIN_VALUE..MAX_VALUE)) {
+                "Invalid brightness level : $brightness. Acceptable values are in range [$MIN_VALUE..$MAX_VALUE]."
+            }
+            return CACHE[brightness]
         }
+
     }
-
-    /** Minimum brightness */
-    public static final Brightness BRIGHTNESS_MIN = of(MIN_VALUE);
-    /** Maximum brightness */
-    public static final Brightness BRIGHTNESS_MAX = of(MAX_VALUE);
-
-    /**
-     * Factory method.
-     *
-     * @param brightness The desired level of brightess. Must be in range [{@link net.thecodersbreakfast.lp4j.api.Brightness#MIN_VALUE},{@link
-     * net.thecodersbreakfast.lp4j.api.Brightness#MAX_VALUE}]
-     * @return The Brightness instance.
-     * @throws java.lang.IllegalArgumentException If the requested brightness level is out of acceptable range.
-     */
-    public static Brightness of(int brightness) {
-        if (brightness < MIN_VALUE || brightness > MAX_VALUE) {
-            throw new IllegalArgumentException("Invalid brightness level : " + brightness + ". Acceptable values are in range [0..15].");
-        }
-        return CACHE[brightness];
-    }
-
-    /** Level of brightness */
-    private final int brightnessLevel;
-
-    /**
-     * Constructor.
-     *
-     * @param brightnessLevel The desired level of brightess.
-     */
-    private Brightness(int brightnessLevel) {
-        this.brightnessLevel = brightnessLevel;
-    }
-
     /**
      * Returns the level of brightness
      *
      * @return the level of brightness
      */
-    public int getBrightnessLevel() {
-        return brightnessLevel;
-    }
 
     /**
      * Returns a brighter Brightness instance, if the maximum level of brightness is not already reached.
      *
      * @return a brighter Brightness instance, or the same instance if it was already the brightest.
      */
-    public Brightness more() {
-        if (brightnessLevel < MAX_VALUE) {
-            return of(brightnessLevel + 1);
+    fun more(): Brightness {
+        return if (brightnessLevel < MAX_VALUE) {
+            of(brightnessLevel + 1)
+        } else {
+            this
         }
-        return this;
     }
 
     /**
@@ -97,27 +84,12 @@ public final class Brightness {
      *
      * @return a less bright Brightness instance, or the same instance if it was already the least bright.
      */
-    public Brightness less() {
-        if (brightnessLevel > MIN_VALUE) {
-            return of(brightnessLevel - 1);
+    fun less(): Brightness {
+        return if (brightnessLevel > MIN_VALUE) {
+            of(brightnessLevel - 1)
+        } else {
+            this
         }
-        return this;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Brightness that = (Brightness) o;
-        return brightnessLevel == that.brightnessLevel;
-    }
-
-    @Override
-    public int hashCode() {
-        return brightnessLevel;
-    }
 }
