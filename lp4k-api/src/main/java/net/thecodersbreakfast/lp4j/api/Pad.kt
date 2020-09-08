bab2bea8075edd13c481f5e387614f251c992bde
vrudas <vrudas4work@gmail.com>
2020-09-08 22:25:23 +0300

    Migrated Pad to Kotlin

--- lp4k-api/src/main/java/net/thecodersbreakfast/lp4j/api/Pad.kt
+++ lp4k-api/src/main/java/net/thecodersbreakfast/lp4j/api/Pad.kt
@@ -1,120 +1,76 @@
 /*
- * Copyright 2015 Olivier Croisier (thecodersbreakfast.net)
+ *    Copyright 2020 Vasyl Rudas
+ *    Copyright 2015 Olivier Croisier (thecodersbreakfast.net)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
  */
-
-package net.thecodersbreakfast.lp4j.api;
+package net.thecodersbreakfast.lp4j.api
 
 /**
  * Represents a square pad on the Launchpad.
  *
- * <p>{@code Pad} instances are immutable and cached.
+ *
+ * `Pad` instances are immutable and cached.
+ *
+ * @param x The X coordinate of the pad.
+ * @param y The Y coordinate of the pad.
  *
  * @author Olivier Croisier (olivier.croisier@gmail.com)
  */
-public class Pad {
+data class Pad(val x: Int, val y: Int) {
 
-    /** Minimal X coordinate for a pad */
-    public static final int X_MIN = 0;
-    /** Maximal X coordinate for a pad */
-    public static final int X_MAX = 7;
-    /** Minimal Y coordinate for a pad */
-    public static final int Y_MIN = 0;
-    /** Maximal Y coordinate for a pad */
-    public static final int Y_MAX = 7;
+    companion object {
 
-    /** Cache of all pads */
-    private static final Pad[][] PADS = new Pad[8][8];
+        /** Minimal X coordinate for a pad  */
+        const val X_MIN = 0
 
-    static {
-        for (int i = X_MIN; i <= X_MAX; i++) {
-            for (int j = Y_MIN; j <= Y_MAX; j++) {
-                PADS[i][j] = new Pad(i, j);
+        /** Maximal X coordinate for a pad  */
+        const val X_MAX = 7
+
+        /** Minimal Y coordinate for a pad  */
+        const val Y_MIN = 0
+
+        /** Maximal Y coordinate for a pad  */
+        const val Y_MAX = 7
+
+        /** Cache of all pads  */
+        private val PADS: Array<Array<Pad>> = initPads()
+
+        private fun initPads(): Array<Array<Pad>> {
+            val padRowSize = X_MAX + 1
+            val padColumnSize = Y_MAX + 1
+
+            return Array(padRowSize) { i ->
+                Array(padColumnSize) { j -> Pad(i, j) }
             }
         }
-    }
 
-    /**
-     * Factory method.
-     *
-     * @param x The X coordinate of the pad. Must be in range [{@link Pad#X_MIN},{@link Pad#X_MAX}].
-     * @param y The Y coordinate of the pad. Must be in range [{@link Pad#Y_MIN},{@link Pad#Y_MAX}].
-     * @return The pad.
-     * @throws java.lang.IllegalArgumentException If the coordinates are invalid.
-     */
-    public static Pad at(int x, int y) {
-        if (x < X_MIN || x > X_MAX || y < Y_MIN || y > Y_MAX) {
-            throw new IllegalArgumentException("Illegal pad coordinates : (" + x + "," + y + "). Acceptable values are in [0..7] on both axis.");
+        /**
+         * Factory method.
+         *
+         * @param x The X coordinate of the pad. Must be in range [[Pad.X_MIN],[Pad.X_MAX]].
+         * @param y The Y coordinate of the pad. Must be in range [[Pad.Y_MIN],[Pad.Y_MAX]].
+         * @return The pad.
+         * @throws java.lang.IllegalArgumentException If the coordinates are invalid.
+         */
+        fun at(x: Int, y: Int): Pad {
+            require(x in (X_MIN..X_MAX) && y in (Y_MIN..Y_MAX)) {
+                "Illegal pad coordinates : ($x,$y). Acceptable values are in [0..7] on both axis."
+            }
+            return PADS[x][y]
         }
-        return PADS[x][y];
+
     }
 
-    /** The X coordinate */
-    private final int x;
-    /** The Y coordinate */
-    private final int y;
-
-    /**
-     * Constructor.
-     *
-     * @param x The X coordinate of the pad.
-     * @param y The Y coordinate of the pad.
-     */
-    private Pad(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    /**
-     * Returns the X coordinate.
-     *
-     * @return The X coordinate.
-     */
-    public int getX() {
-        return x;
-    }
-
-    /**
-     * Returns the Y coordinate.
-     *
-     * @return The Y coordinate
-     */
-    public int getY() {
-        return y;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Pad pad = (Pad) o;
-        return x == pad.x && y == pad.y;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = x;
-        result = 31 * result + y;
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Pad[" + x + ',' + y + ']';
-    }
 }
--- lp4k-api/src/test/java/net/thecodersbreakfast/lp4j/api/PadTest.kt
+++ lp4k-api/src/test/java/net/thecodersbreakfast/lp4j/api/PadTest.kt
@@ -1,114 +1,94 @@
 /*
- * Copyright 2020 Vasyl Rudas
- * Copyright 2015 Olivier Croisier (thecodersbreakfast.net)
+ *    Copyright 2020 Vasyl Rudas
+ *    Copyright 2015 Olivier Croisier (thecodersbreakfast.net)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
  */
+package net.thecodersbreakfast.lp4j.api
 
-package net.thecodersbreakfast.lp4j.api;
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.util.*
 
-import org.junit.jupiter.api.Test;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-
-public class PadTest {
+class PadTest {
 
     @Test
-    public void at() {
-        Pad pad = Pad.at(Pad.X_MIN, Pad.Y_MIN);
-        assertNotNull(pad);
-        assertEquals(Pad.X_MIN, pad.getX());
-        assertEquals(Pad.Y_MIN, pad.getY());
+    fun at() {
+        val pad = Pad.at(Pad.X_MIN, Pad.Y_MIN)
+        assertEquals(Pad.X_MIN, pad.x)
+        assertEquals(Pad.Y_MIN, pad.y)
     }
 
     @Test
-    public void check_pad_coordinates_for_different_input_values() {
-        Pad pad = Pad.at(1, 2);
-        assertNotNull(pad);
-        assertEquals(1, pad.getX());
-        assertEquals(2, pad.getY());
+    fun check_pad_coordinates_for_different_input_values() {
+        val pad = Pad.at(1, 2)
+        assertEquals(1, pad.x)
+        assertEquals(2, pad.y)
     }
 
     @Test
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    public void at_xTooLow() {
-        assertThrows(IllegalArgumentException.class, () -> Pad.at(Pad.X_MIN - 1, Pad.Y_MIN));
+    fun at_xTooLow() {
+        assertThrows<IllegalArgumentException> { Pad.at(Pad.X_MIN - 1, Pad.Y_MIN) }
     }
 
     @Test
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    public void at_xTooHigh() {
-        assertThrows(IllegalArgumentException.class, () -> Pad.at(Pad.X_MAX + 1, Pad.Y_MIN));
+    fun at_xTooHigh() {
+        assertThrows<IllegalArgumentException> { Pad.at(Pad.X_MAX + 1, Pad.Y_MIN) }
     }
 
     @Test
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    public void at_yTooLow() {
-        assertThrows(IllegalArgumentException.class, () -> Pad.at(Pad.X_MIN, Pad.Y_MIN - 1));
+    fun at_yTooLow() {
+        assertThrows<IllegalArgumentException> { Pad.at(Pad.X_MIN, Pad.Y_MIN - 1) }
     }
 
     @Test
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    public void at_yTooHigh() {
-        assertThrows(IllegalArgumentException.class, () -> Pad.at(Pad.X_MIN, Pad.Y_MAX + 1));
+    fun at_yTooHigh() {
+        assertThrows<IllegalArgumentException> { Pad.at(Pad.X_MIN, Pad.Y_MAX + 1) }
     }
 
     @Test
-    public void tostring() {
-        Pad pad = Pad.at(Pad.X_MIN, Pad.Y_MIN);
-        assertEquals("Pad[0,0]", pad.toString());
-    }
-
-    @Test
-    @SuppressWarnings("java:S3415")
-    void check_equals() {
+    fun check_equals() {
         assertEquals(
             Pad.at(Pad.X_MIN, Pad.Y_MIN),
             Pad.at(Pad.X_MIN, Pad.Y_MIN)
-        );
+        )
     }
 
     @Test
-    @SuppressWarnings("java:S3415")
-    void not_equal() {
+    fun not_equal() {
         assertNotEquals(
             Pad.at(Pad.X_MIN, Pad.Y_MIN),
             Pad.at(Pad.X_MAX, Pad.Y_MAX)
-        );
+        )
     }
 
     @Test
-    @SuppressWarnings("java:S3415")
-    void not_equal_for_different_type() {
-        assertNotEquals(Pad.at(Pad.X_MAX, Pad.Y_MAX), Pad.X_MIN);
+    fun not_equal_for_different_type() {
+        assertNotEquals(Pad.at(Pad.X_MAX, Pad.Y_MAX), Pad.X_MIN)
     }
 
     @Test
-    @SuppressWarnings("java:S3415")
-    void not_equal_for_null() {
-        assertNotEquals(Pad.at(Pad.X_MIN, Pad.Y_MIN), null);
+    fun not_equal_for_null() {
+        assertNotEquals(Pad.at(Pad.X_MIN, Pad.Y_MIN), null)
     }
 
     @Test
-    void hashcode_is_equal_for_pads_with_same_coordinates() {
-        Set<Pad> pads = new HashSet<>();
-        pads.add(Pad.at(Pad.X_MAX, Pad.Y_MAX));
-        pads.add(Pad.at(7, 7));
-
-        assertEquals(1, pads.size());
+    fun hashcode_is_equal_for_pads_with_same_coordinates() {
+        val pads: MutableSet<Pad?> = HashSet()
+        pads.add(Pad.at(Pad.X_MAX, Pad.Y_MAX))
+        pads.add(Pad.at(7, 7))
+        assertEquals(1, pads.size)
     }
 }
